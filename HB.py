55e4c17b913e8bd7e055119f51070b6c3c335045
THE HB <82237720+hbbots@users.noreply.github.com>
2023-10-12 15:15:32 +0530

    Update HB.py

--- HB.py
+++ HB.py
@@ -8,7 +8,7 @@ from pyrogram.types import Message
 
 
 HB = Client(
-    "MSG_DELETING Bot",
+    "TEXT_TO_FILE Bot",
     bot_token = os.environ["BOT_TOKEN"],
     api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"],
